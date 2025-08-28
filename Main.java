918568b0d97677aa50a2cd3ee15d7830bdc6c59b
ianxjasper <142211105+ianxjasper@users.noreply.github.com>
2025-08-28 15:53:15 +0000

    Task 2 Complete

--- Main.java
+++ Main.java
@@ -1,1 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Java!");
 
+        char h = 'H';
+        byte e = 3;
+        short ll = 110;
+        char w = 'w';
+        int o = 0;
+        char r = 'r';
+        int l = 1;
+        char d = 'd';
+        float version = 2.0F;
+        boolean t = true;
+        String helloWorld = "" + h + e + ll + " " + w + o + r + l + d + " " + version + " " + t;
+        System.out.println(helloWorld);
+    }
+}
